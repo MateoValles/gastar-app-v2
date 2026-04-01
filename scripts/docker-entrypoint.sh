7a6db8f273cdb41af48672637c2508c500db5832
Mateo Valles <137822683+MValles02@users.noreply.github.com>
2026-04-01 20:29:02 -0300

    fix(deployment): wrong dockerfile packages

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -eu
 
 echo "[entrypoint] Running Prisma migrations..."
-pnpm exec prisma migrate deploy --schema=database/prisma/schema.prisma
+./node_modules/.bin/prisma migrate deploy --schema=database/prisma/schema.prisma
 
 echo "[entrypoint] Starting backend..."
 exec node packages/backend/dist/index.js
